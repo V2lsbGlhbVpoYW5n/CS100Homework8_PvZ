b0de20c0be32e30b53c71b63b085aa3fe70f3561
V2lsbGlhbVpoYW5n <150314094+V2lsbGlhbVpoYW5n@users.noreply.github.com>
2024-06-19 17:37:29 +0800

    Background create

--- src/GameWorld/GameWorld.cpp
+++ src/GameWorld/GameWorld.cpp
@@ -1,7 +1,10 @@
 #include "GameWorld.hpp"
+#include "Background.hpp"
 
 GameWorld::GameWorld() {
-
+    gameObjects.push_back(
+        std::make_shared<Background>(IMGID_BACKGROUND, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, LAYER_BACKGROUND,
+                                     WINDOW_WIDTH, WINDOW_HEIGHT, ANIMID_NO_ANIMATION));
 }
 
 GameWorld::~GameWorld() {
@@ -9,6 +12,9 @@ GameWorld::~GameWorld() {
 
 void GameWorld::Init() {
     // YOUR CODE HERE
+    for (auto& obj : gameObjects) {
+
+    }
 }
 
 LevelStatus GameWorld::Update() {
